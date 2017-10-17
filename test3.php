548bebf2cedef62430bb6b3c6e9a734e6d848db3
huang <1169078896@qq.com>
2017-10-17 14:59:48 +0800

    增加功能2

---
+++ test3.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: A
+ * Date: 2017/10/17
+ * Time: 10:43
+ */
+
+echo 234234;
+echo 3423;
---
+++ test5.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: A
+ * Date: 2017/10/17
+ * Time: 14:58
+ */
+
+echo "增加功能2";
